c36e65c63c5803c95b92e1e5561442186d1b5c04
João Damasceno <id629631@outlook.com>
2023-12-30 12:27:57 -0300

    2.27

--- src/main/java/com/error404/errorfoodapi/di/notificacao/mock/NotificadorMock.java
+++ src/main/java/com/error404/errorfoodapi/di/notificacao/mock/NotificadorMock.java
@@ -1,4 +1,4 @@
-package com.error404.errorfoodapi.di.notificacao;
+package com.error404.errorfoodapi.di.notificacao.mock;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -6,21 +6,24 @@ import org.springframework.stereotype.Component;
 
 import com.error404.errorfoodapi.di.modelo.Cliente;
 import com.error404.errorfoodapi.di.modelo.enums.NivelUrgencia;
+import com.error404.errorfoodapi.di.notificacao.NotificacaPropreties;
+import com.error404.errorfoodapi.di.notificacao.Notificador;
+import com.error404.errorfoodapi.di.notificacao.TipoNotificador;
 
-@Profile("prod")
+
+@Profile("dev")
 @TipoNotificador(NivelUrgencia.URGENTE)
 @Component
-public class NotificadorEmail implements Notificador {
-
+public class NotificadorMock implements Notificador {
 	@Autowired
 	private NotificacaPropreties propreties;
-		
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.println("Host: "+propreties.getHostServidor());
 		System.out.println("Porta: "+ propreties.getPortaServidor());
 
-		System.out.printf("Notificando %s através do e-mail  %s: %s\n", 
+		System.out.printf("Mock: Notificando %s através do e-mail  %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(),mensagem);
 	}
 
